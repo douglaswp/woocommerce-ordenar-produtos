f8d24d6fa0f64b2626c033d4db35fa1247f19185
douglaswp <douglaswp@users.noreply.github.com>
2018-01-25 20:46:13 -0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Ordenando produtos por quantidade em estoque, Produtos disponíveis aparecem primeiro
+ */
+class iWC_Orderby_Stock_Status {
+    public function __construct()
+    {
+        // Check if WooCommerce is active
+        if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+            add_filter('posts_clauses', array($this, 'order_by_stock_status'), 2000);
+        }
+    }
+    public function order_by_stock_status($posts_clauses)
+    {
+        global $wpdb;
+        // only change query on WooCommerce loops
+        if (is_woocommerce() && (is_shop() || is_product_category() || is_product_tag())) {
+            $posts_clauses['join'] .= " INNER JOIN $wpdb->postmeta istockstatus ON ($wpdb->posts.ID = istockstatus.post_id) ";
+            $posts_clauses['orderby'] = " istockstatus.meta_value ASC, " . $posts_clauses['orderby'];
+            $posts_clauses['where'] = " AND istockstatus.meta_key = '_stock_status' AND istockstatus.meta_value <> '' " . $posts_clauses['where'];
+        }
+        return $posts_clauses;
+    }
+}
+new iWC_Orderby_Stock_Status;
