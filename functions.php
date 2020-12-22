619a884154016ce4dbfc35291d089d6601124179
douglaswp <douglaswp@users.noreply.github.com>
2020-12-22 15:41:52 -0300

    Atualização WC 4.8.0
    Mudanças para checar compatibilidade e reorganização de código

--- functions.php
+++ functions.php
@@ -1,20 +1,16 @@
 <?php
 
-/**
- * Ordenando produtos por quantidade em estoque, Produtos disponíveis aparecem primeiro
- */
-class iWC_Orderby_Stock_Status {
-    public function __construct()
-    {
-        // Check if WooCommerce is active
+// Ordenando produtos por quantidade em estoque, Produtos disponíveis aparecem primeiro
+class DWP_Orderby_Stock_Status {
+    public function __construct(){
+        // Checando se o Woocommerce está instalado e ativo
         if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
             add_filter('posts_clauses', array($this, 'order_by_stock_status'), 2000);
         }
     }
-    public function order_by_stock_status($posts_clauses)
-    {
+    public function order_by_stock_status($posts_clauses){
         global $wpdb;
-        // only change query on WooCommerce loops
+        // Altero a ordem apenas na listagem dos produtos
         if (is_woocommerce() && (is_shop() || is_product_category() || is_product_tag())) {
             $posts_clauses['join'] .= " INNER JOIN $wpdb->postmeta istockstatus ON ($wpdb->posts.ID = istockstatus.post_id) ";
             $posts_clauses['orderby'] = " istockstatus.meta_value ASC, " . $posts_clauses['orderby'];
@@ -23,4 +19,4 @@ class iWC_Orderby_Stock_Status {
         return $posts_clauses;
     }
 }
-new iWC_Orderby_Stock_Status;
+new DWP_Orderby_Stock_Status;
